2f329fc1100421c14842beb17655d6a32653b29c
lwsnz <lwsnz@users.noreply.github.com>
2022-04-27 13:10:19 +1200

    corrected web-request invoked

--- fortiguard-webfilter-lookup.ps1
+++ fortiguard-webfilter-lookup.ps1
@@ -1,6 +1,9 @@
-$urls=gc "url-blacklist-scan.txt"
+#$urls=gc "url-blacklist-scan.txt"
+$urls=gc "$env:USERPROFILE\Documents\GitHub\web-filter-lookup\url-blacklist-scan.txt"
+
 foreach($url in $urls){
-$web = Invoke-WebRequest $url 
+$web = Invoke-WebRequest https://www.fortiguard.com/webfilter?q=$url 
+# Write to console with Write-Host next not log file output
 Write-Host "$url"
 $web.tostring() -split "[`r`n]" | select-string ">Category: "
 Write-Host "------"
